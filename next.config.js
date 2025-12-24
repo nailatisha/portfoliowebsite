5b9055c368d4ebdf8f05ff7d04bb0e44ac5f82d6
nailatisha <nailatisha8@gmail.com>
2025-12-24 21:39:49 +0700

    update config untuk github pages

--- next.config.js
+++ next.config.js
@@ -1,10 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
+  output: 'export', 
   images: {
-    domains: ['localhost'],
+    unoptimized: true, 
   },
-}
+};
 
-module.exports = nextConfig
-
+export default nextConfig;
